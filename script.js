9935923419a3d4c3804f9ee9acf5381f67aef948
bobby <robionbarnabas18@gmail.com>
2026-02-08 20:57:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const toggleBtn = document.getElementById('toggleMode');
 const body = document.body;
 const icon = toggleBtn.querySelector('i');
-const floatingToggle = document.getElementById('floatingToggle');
 
-// Toggle
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    if (body.classList.contains('light-mode')) {
+        icon.className = 'fas fa-sun'; // Change to sun icon for light mode
+    } else {
+        icon.className = 'fas fa-moon'; // Change to moon icon for dark mode
+    }
+});
